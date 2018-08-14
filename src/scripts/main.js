6f1b9f5b13043b3251a317cb4683ab3b481ed333
Akeen11 <aartkeen@ut.utm.edu>
2018-08-14 09:39:06 -0500

    cleaned up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,10 @@
-console.log("Welcome to Browserify")
-//const $=require("jquery")
-const DataManager=require("./DataManager")
+const DataManager = require("./DataManager")
 const LoginFormManager = require("./login")
 const $ = require("jquery")
 
 $("#loginForm").html(LoginFormManager.renderLoginForm())
 
-$("#loginForm").on("click", event => {
-    if (event.target.classList.contains("LoginButton"))
-        (console.log("IT WORKS"))
+$("#loginForm").on("click", "#LoginButton", event => {
+    (console.log("IT WORKS"))
 })
 
